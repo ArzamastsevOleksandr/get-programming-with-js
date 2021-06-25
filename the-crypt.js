221585fdbc9f79e78a1b59244634b3a6148df1dc
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-06-25 16:31:45 +0300

    feat: add a Player constructor function

--- the-crypt.js
+++ the-crypt.js
@@ -1,54 +1,65 @@
-const ITEMS = 'Items:'
-const EXITS = 'Exits from '
-const LEFT_BORDER_PLAYER = '* '
-const RIGHT_BORDER_PLAYER = ' *'
 const BORDER_SYMBOL_PLAYER = '*'
-const ITEMS_LEFT_BORDER = ' '.repeat(2)
-const ITEM_SEPARATOR = '- ';
-const EXITS_LEFT_BORDER = ' '.repeat(2)
-const EXIT_SEPARATOR = '- ';
+const SPACE = ' '
 
-const getBorder = (quantityOfChars) => wrap(BORDER_SYMBOL_PLAYER.repeat(quantityOfChars), '**', '**')
+const LEFT_BORDER_PLAYER = BORDER_SYMBOL_PLAYER + SPACE.repeat(1)
+const RIGHT_BORDER_PLAYER = SPACE.repeat(1) + BORDER_SYMBOL_PLAYER
 
-const getPlayerLocationDescription = player => player.name + ' is in ' + player.place
+const ITEMS = 'Items:'
+const ITEMS_LEFT_BORDER = SPACE.repeat(2)
+const DASH = '-'
+const ITEM_SEPARATOR = DASH + SPACE.repeat(1)
 
-const getPlayerHealthDescription = player => player.name + ' has health ' + player.health
+const EXITS = 'Exits from '
+const EXITS_LEFT_BORDER = SPACE.repeat(2)
+const EXIT_SEPARATOR = DASH + SPACE.repeat(1)
 
-const getPlayerItemsString = items => {
-    return ITEMS_LEFT_BORDER + ITEMS + '\n'
-        + items.map(i => ITEMS_LEFT_BORDER + ITEM_SEPARATOR + i).join('\n')
-}
+const getBorder = (symbol, quantityOfChars) => symbol.repeat(quantityOfChars)
 
-const padWithSpaces = (string, maxWidth) => string + ' '.repeat(maxWidth - string.length)
+const padWithSpaces = (string, maxWidth) => string + SPACE.repeat(maxWidth - string.length)
 
 const wrap = (string, left, right) => left + string + right
 
-const displayPlayer = function (player) {
-    const playerLocationDescription = getPlayerLocationDescription(player)
-    const playerHealthDescription = getPlayerHealthDescription(player)
+const Player = function (name, health) {
+    this.name = name
+    this.health = health
+    this.items = []
+    this.place = null
 
-    const maxWidth = Math.max(playerLocationDescription.length, playerHealthDescription.length)
+    this.getLocationDescription = player => player.name + ' is in ' + player.place
 
-    const playerDescription = getBorder(maxWidth) + '\n'
-        + wrap(padWithSpaces(player.name, maxWidth), LEFT_BORDER_PLAYER, RIGHT_BORDER_PLAYER) + '\n'
-        + getBorder(maxWidth) + '\n'
-        + wrap(padWithSpaces(playerLocationDescription, maxWidth), LEFT_BORDER_PLAYER, RIGHT_BORDER_PLAYER) + '\n'
-        + wrap(padWithSpaces(playerHealthDescription, maxWidth), LEFT_BORDER_PLAYER, RIGHT_BORDER_PLAYER) + '\n'
-        + getBorder(maxWidth) + '\n'
-        + getPlayerItemsString(player.items) + '\n'
-        + getBorder(maxWidth) + '\n'
+    this.getHealthDescription = player => player.name + ' has health ' + player.health
 
-    console.log(playerDescription)
+    this.getBorder = (quantityOfChars) => wrap(getBorder(BORDER_SYMBOL_PLAYER, quantityOfChars), '**', '**')
+
+    this.getItemsDescription = items => {
+        return ITEMS_LEFT_BORDER + ITEMS + '\n'
+            + items.map(i => ITEMS_LEFT_BORDER + ITEM_SEPARATOR + i).join('\n')
+    }
+
+    this.getDescription = () => {
+        const playerLocationDescription = this.getLocationDescription(player)
+        const playerHealthDescription = this.getHealthDescription(player)
+
+        const maxWidth = Math.max(playerLocationDescription.length, playerHealthDescription.length)
+
+        return this.getBorder(maxWidth) + '\n'
+            + wrap(padWithSpaces(player.name, maxWidth), LEFT_BORDER_PLAYER, RIGHT_BORDER_PLAYER) + '\n'
+            + this.getBorder(maxWidth) + '\n'
+            + wrap(padWithSpaces(playerLocationDescription, maxWidth), LEFT_BORDER_PLAYER, RIGHT_BORDER_PLAYER) + '\n'
+            + wrap(padWithSpaces(playerHealthDescription, maxWidth), LEFT_BORDER_PLAYER, RIGHT_BORDER_PLAYER) + '\n'
+            + this.getBorder(maxWidth) + '\n'
+            + this.getItemsDescription(player.items) + '\n'
+            + this.getBorder(maxWidth) + '\n'
+    }
+
+    this.addItems = (...items) => items.forEach(item => this.items.push(item))
 }
 
-const player = {
-    name: 'Kandra',
-    health: 50,
-    place: 'The Dungeon of Doom',
-    items: ['a rusty key', 'a trusty lamp']
-}
+const player = new Player('Kandra', 50)
+player.addItems('a rusty key', 'a trusty lamp')
+player.place = 'The Dungeon of Doom'
 
-displayPlayer(player)
+console.log(player.getDescription())
 
 const Place = function (title, description) {
     this.title = title
@@ -56,34 +67,22 @@ const Place = function (title, description) {
     this.items = []
     this.exits = []
 
-    this.getInfo = function () {
-        return this.title + '\n'
-            + this.description + '\n'
-            + this.getItemsString() + '\n'
-            + this.getExitsString()
-    }
+    this.getInfo = () => this.title + '\n'
+        + this.description + '\n'
+        + this.getItemsString() + '\n'
+        + this.getExitsString()
 
-    this.getItemsString = function () {
-        return ITEMS + '\n'
-            + this.items.map(i => ITEMS_LEFT_BORDER + ITEM_SEPARATOR + i).join('\n')
-    }
+    this.getItemsString = () => ITEMS + '\n'
+        + this.items.map(i => ITEMS_LEFT_BORDER + ITEM_SEPARATOR + i).join('\n')
 
-    this.getExitsString = function () {
-        return EXITS + this.title + ':' + '\n'
-            + this.exits.map(i => EXITS_LEFT_BORDER + EXIT_SEPARATOR + i).join('\n')
-    }
+    this.getExitsString = () => EXITS + this.title + ':' + '\n'
+        + this.exits.map(i => EXITS_LEFT_BORDER + EXIT_SEPARATOR + i).join('\n')
 
-    this.addItem = function (item) {
-        this.items.push(item)
-    }
+    this.addItem = item => this.items.push(item)
 
-    this.addExits = function (...exits) {
-        exits.forEach(exit => this.exits.push(exit))
-    }
+    this.addExits = (...exits) => exits.forEach(exit => this.exits.push(exit))
 
-    this.toString = function () {
-        return this.title
-    }
+    this.toString = () => this.title
 }
 
 const library = new Place(
