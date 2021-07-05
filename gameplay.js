39f9c4324ee0ba56fc1af9979bf74b8dcbcfc907
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2021-07-05 18:39:56 +0300

    feat: implement challenges on the map

--- gameplay.js
+++ gameplay.js
@@ -1,8 +1,10 @@
 const {getGame} = require("./the-crypt");
 
-const game = getGame()
-game.get()
+const game = getGame('John')
 game.go('south')
+game.use('holy water', 'south')
+game.go('south')
+
 game.go('north')
 game.go('west')
 game.go('bugaga')
